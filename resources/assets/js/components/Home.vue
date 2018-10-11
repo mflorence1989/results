067eed67eb0f68e1a269b248b262ab2d4c7b2c18
John Doe <mario.florence215@gmail.com>
2018-10-11 07:39:30 -0700

    new

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -13,11 +13,7 @@
   <div class="contact-info">
   <div class="container">
   <div class="booking">Philadelphia,Pa</div>
-<<<<<<< HEAD
   <h2> 267 - 748 - 7107</h2>
-=======
-  <h2>267 - 748 - 7107</h2>
->>>>>>> bfe71033f7194a4ad9b4ccb44a453968963baab8
 
       </div>
       </div>
